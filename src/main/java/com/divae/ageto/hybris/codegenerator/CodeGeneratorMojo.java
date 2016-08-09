26df6d5b77b521254ecbfbded93f34e077049a97
Klaus Hauschild <klaus.hauschild.1984@gmail.com>
2016-08-09 14:27:15 +0200

    replace deprecated @Component with @Paramater

--- src/main/java/com/divae/ageto/hybris/codegenerator/CodeGeneratorMojo.java
+++ src/main/java/com/divae/ageto/hybris/codegenerator/CodeGeneratorMojo.java
@@ -3,9 +3,9 @@ package com.divae.ageto.hybris.codegenerator;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
-import org.apache.maven.plugins.annotations.Component;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.project.MavenProject;
 
 /**
@@ -14,8 +14,8 @@ import org.apache.maven.project.MavenProject;
 @Mojo(name = "code-generator", defaultPhase = LifecyclePhase.GENERATE_SOURCES)
 public class CodeGeneratorMojo extends AbstractMojo {
 
-    @Component
-    private MavenProject project;
+    @Parameter(defaultValue = "${project}", readonly = true)
+    MavenProject project;
 
     @Override
     public void execute() throws MojoExecutionException, MojoFailureException {
