aaf03c2d934c5466c18147b51b030191acfbf8cb
Klaus Hauschild <klaus.hauschild.1984@gmail.com>
2016-08-11 19:26:37 +0200

    Remove PomCreator #12
    now the pom creation functionality is back at CreatePomTask

--- src/main/java/com/divae/ageto/hybris/install/task/CreatePomTask.java
+++ src/main/java/com/divae/ageto/hybris/install/task/CreatePomTask.java
@@ -1,7 +1,15 @@
 package com.divae.ageto.hybris.install.task;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
+import java.io.InputStreamReader;
+import java.util.List;
 import java.util.Map;
+
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
 
 /**
  * @author Klaus Hauschild
@@ -20,38 +28,35 @@ public class CreatePomTask extends AbstractWorkDirectoryTask {
 
     @Override
     protected void execute(final TaskContext taskContext, final File workDirectory) {
-        // PomCreator.create(taskContext.getHybrisVersion().getVersion(), parameters, template, workDirectory, targetDirectory);
+        final Map<String, String> token = Maps.newHashMap();
+        token.putAll(parameters);
+        token.put("hybris.version", taskContext.getHybrisVersion().getVersion());
+        try (final BufferedReader reader = new BufferedReader(
+                new TokenReplacingReader(new InputStreamReader(ClassLoader.getSystemResourceAsStream(template)), token))) {
 
+            final List<String> lines = Lists.newArrayList();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                lines.add(line);
+            }
 
-        // final Map<String, String> token = Maps.newHashMap();
-        // token.putAll(parameters);
-        // token.put("hybris.version", taskContext.getHybrisVersion().getVersion());
-        // try (final BufferedReader reader = new BufferedReader(
-        // new TokenReplacingReader(new InputStreamReader(ClassLoader.getSystemResourceAsStream(template)), token))) {
-        //
-        // final List<String> lines = Lists.newArrayList();
-        // String line;
-        // while ((line = reader.readLine()) != null) {
-        // lines.add(line);
-        // }
-        //
-        // final File directory = new File(workDirectory, targetDirectory);
-        // directory.mkdirs();
-        // final File targetFile = new File(directory, "pom.xml");
-        // targetFile.createNewFile();
-        // try (final BufferedWriter writer = new BufferedWriter(new FileWriter(targetFile))) {
-        // for (final String lineToWrite : lines) {
-        // try {
-        // writer.write(lineToWrite);
-        // writer.newLine();
-        // } catch (final Exception exception) {
-        // throw new RuntimeException(exception);
-        // }
-        // }
-        // }
-        //
-        // } catch (final Exception exception) {
-        // throw new RuntimeException(exception);
-        // }
+            final File directory = new File(workDirectory, targetDirectory);
+            directory.mkdirs();
+            final File targetFile = new File(directory, "pom.xml");
+            targetFile.createNewFile();
+            try (final BufferedWriter writer = new BufferedWriter(new FileWriter(targetFile))) {
+                for (final String lineToWrite : lines) {
+                    try {
+                        writer.write(lineToWrite);
+                        writer.newLine();
+                    } catch (final Exception exception) {
+                        throw new RuntimeException(exception);
+                    }
+                }
+            }
+
+        } catch (final Exception exception) {
+            throw new RuntimeException(exception);
+        }
     }
 }
