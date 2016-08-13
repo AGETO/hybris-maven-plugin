68d4f5f6e7e42a7725c759da1018988d8c2316de
Klaus Hauschild <klaus.hauschild.1984@gmail.com>
2016-08-13 22:14:45 +0200

    cleanup and codestyle

--- src/main/java/com/divae/ageto/hybris/install/task/metadata/ExtensionMetadataFile.java
+++ src/main/java/com/divae/ageto/hybris/install/task/metadata/ExtensionMetadataFile.java
@@ -19,44 +19,39 @@ import com.google.common.base.Throwables;
 public enum ExtensionMetadataFile {
     ;
 
-    private static String fileNameFormat = "%s-metadata.properties";
+    private static String FILE_NAME_FORMAT = "%s-metadata.properties";
 
     public static File createMetadataFile(final Extension extension, final File workDirectory) {
-        File metadataFile = new File(new File(workDirectory, extension.getName()),
+        final File metadataFile = new File(new File(workDirectory, extension.getName()),
                 getMetadataFileName(extension.getName()).toString());
-
-        Properties properties = new Properties();
-
+        final Properties properties = new Properties();
         try {
             properties.setProperty("extension.name", extension.getName());
             properties.setProperty("extension.directory", extension.getBaseDirectory().toString());
             addExtensionBinaryProperties(properties, extension);
             properties.store(new FileOutputStream(metadataFile), null);
-        } catch (IOException e) {
-            Throwables.propagate(e);
+            return metadataFile;
+        } catch (IOException exception) {
+            throw Throwables.propagate(exception);
         }
-
-        return metadataFile;
     }
 
     public static ExtensionProperties readMetadataFile(final File workDirectory, final String extensionName) {
-        File metadataFile = new File(new File(workDirectory, extensionName), getMetadataFileName(extensionName).toString());
-
+        final File metadataFile = new File(new File(workDirectory, extensionName), getMetadataFileName(extensionName).toString());
         Properties properties = new Properties();
         try {
             properties.load(new FileInputStream(metadataFile));
-            String name = properties.getProperty("extension.name");
-            File baseFile = new File(properties.getProperty("extension.directory"));
-            ExtensionBinary binary = getExtensionBinary(properties);
+            final String name = properties.getProperty("extension.name");
+            final File baseFile = new File(properties.getProperty("extension.directory"));
+            final ExtensionBinary binary = getExtensionBinary(properties);
             return new ExtensionProperties(baseFile);
-        } catch (IOException e) {
-            Throwables.propagate(e);
+        } catch (final IOException exception) {
+            throw Throwables.propagate(exception);
         }
-        return null;
     }
 
-    private static ExtensionBinary getExtensionBinary(Properties properties) {
-        String type = properties.getProperty("extension.binary.type");
+    private static ExtensionBinary getExtensionBinary(final Properties properties) {
+        final String type = properties.getProperty("extension.binary.type");
         if (type.equals(new None().getType())) {
             return new None();
         }
@@ -66,11 +61,10 @@ public enum ExtensionMetadataFile {
         if (type.equals(new ClassFolder(new File("")).getType())) {
             return new ClassFolder(new File(properties.getProperty("extension.binary.path")));
         }
-
         throw new RuntimeException("Invalid type: " + type);
     }
 
-    private static void addExtensionBinaryProperties(Properties config, Extension extension) {
+    private static void addExtensionBinaryProperties(final Properties config, final Extension extension) {
         config.setProperty("extension.binary.type", extension.getBinary().getType());
         if (extension.getBinary().getClass() != None.class) {
             config.setProperty("extension.binary.path", extension.getBinary().getExtensionBinaryPath().toString());
@@ -78,6 +72,7 @@ public enum ExtensionMetadataFile {
     }
 
     private static File getMetadataFileName(final String extensionName) {
-        return new File(String.format(fileNameFormat, extensionName));
+        return new File(String.format(FILE_NAME_FORMAT, extensionName));
     }
+
 }
